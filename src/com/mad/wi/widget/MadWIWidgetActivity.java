1e767748bc4a21c04af21b850f7c5e79fb929af3
Radosalaw Maciaszczyk <rmaciaszczyk@wi.zut.edu.pl>
2012-04-17 09:26:58 +0200

    Widget Skeleton

---
+++ src/com/mad/wi/widget/MadWIWidgetActivity.java
@@ -0,0 +1,14 @@
+package com.mad.wi.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class MadWIWidgetActivity extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+    }
+
+}
